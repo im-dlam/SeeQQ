81e71490d673eb2a7e43aec45690be5a1e78e5c5
Lam <ldl.contact.booking@gmail.com>
2025-08-19 21:20:25 +0700

    feat: add database use sqlalchemy

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from app.api.v1 import note
 from fastapi.staticfiles import StaticFiles
+from app.database.database import engine
+from app.models import models
 
+models.Base.metadata.create_all(engine)
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="app/static") , name="static")
 app.include_router(note.router)
---
+++ app/models/models.py
@@ -0,0 +1,23 @@
+import datetime
+from app.database.database import Base
+from sqlalchemy import Column, String , Integer , ForeignKey , Date
+from sqlalchemy.orm import relationship
+
+class Notes(Base):
+    __tablename__ = 'notes'
+    id = Column(Integer, primary_key=True, index=True)
+    note = Column(String)
+    datetime = Column(Date)
+    user_id = Column(Integer, ForeignKey('users.id')) # Khóa ngoại của bảng users
+
+    creator = relationship('User',back_populates='notes')
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    username = Column(String)
+    password =  Column(String)
+
+    notes = relationship('Blog', back_populates='creator')
